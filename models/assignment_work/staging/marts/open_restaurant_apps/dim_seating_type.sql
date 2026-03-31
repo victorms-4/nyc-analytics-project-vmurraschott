55562b94beea7134c7d799caebe8fbaa4d94c988
Victor Murra Schott <victor.murraschott@gmail.com>
2026-03-31 01:01:15 +0000

    Added dim_seating_type table

---
+++ models/assignment_work/staging/marts/open_restaurant_apps/dim_seating_type.sql
@@ -0,0 +1,32 @@
+-- Seating type dimension for open restaurant seating applications
+WITH seating_types AS (
+   SELECT DISTINCT
+       seating_interest_sidewalk AS seating_interest,
+        CASE WHEN approved_for_sidewalk_seating = 'Yes' THEN TRUE
+                ELSE FALSE
+            END AS approved_for_sidewalk,
+
+        CASE WHEN approved_for_roadway_seating = 'Yes' THEN TRUE
+           ELSE FALSE
+       END AS approved_for_roadway
+
+--NOTE: The final result we want to select here is two boolean columns (TRUE or FALSE values in them), one column approved_for_sidewalk (TRUE or FALSE value), and one column approved_for_roadway
+   FROM {{ ref('stg_nyc_open_restaurant_apps') }}
+   WHERE seating_interest_sidewalk IS NOT NULL
+),
+seating_dimension AS (
+   SELECT
+       {{ dbt_utils.generate_surrogate_key([
+           'seating_interest',
+           'approved_for_sidewalk',
+           'approved_for_roadway'
+       ]) }} AS seating_type_key,
+
+       seating_interest,
+       approved_for_sidewalk,
+       approved_for_roadway
+
+   FROM seating_types
+)
+
+SELECT * FROM seating_dimension
